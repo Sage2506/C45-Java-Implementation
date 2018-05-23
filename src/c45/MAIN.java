cbe122b6e34fc1908b57bf34a8e135a7e5a852f2
German <german2506.gs@gmail.com>
2018-05-23 14:59:38 -0600

    comentarios en Main

--- src/c45/MAIN.java
+++ src/c45/MAIN.java
@@ -9,8 +9,11 @@ public class MAIN {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		List<List<String>> dataset = new ArrayList<>();
+		// Generar una lista de listas para guardar los valores de los atributos y la clase
 		dataset = Methods.generateDatasetFromFile("datasets/outlooktestmsdos.csv");
+		// Crear la tabla a partir del dataset
 		Table table = new Table(dataset);
+		// Comenzar algoritmo C45
 		Methods.calculaC45(table);
 	}
 }
